67dc2b6bb6f64c62e5d1c9efcc6d28c6d48b90eb
amandanalesso <nalessoamanda@gmail.com>
2020-01-21 10:16:22 -0300

    changes in label intro

--- resources/views/layout/_includes/topo.blade.php
+++ resources/views/layout/_includes/topo.blade.php
@@ -16,7 +16,7 @@
   <nav>
    <div class="nav-wrapper  deep-purple">
      <div class="container">
-       <a href="#!" class="brand-logo">Curso de Laravel</a>
+       <a href="#!" class="brand-logo">Developer Courses</a>
        <a href="#" data-activates="mobile" class="button-collapse"><i class="material-icons">menu</i></a>
        <ul class="right hide-on-med-and-down">
          <li><a href="/">Home</a></li>
